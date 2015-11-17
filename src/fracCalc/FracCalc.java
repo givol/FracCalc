18a34460909e8a5e22523ddaa949eb33fbc92edf
Grant <bossgivol@gmail.com>
2015-11-17 08:56:03 -0500

    I dont understand how i am suppose to preform the equation that converts the number into a int, simplifies it, then puts it back into a string

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -1,11 +1,49 @@
 package fracCalc;
 
+import java.util.Scanner;
+
+public class javatest3 {
+    static int num1 = 0;
+    static int num2 = 0;
+    static int denom1 = 0;
+    static int denom2 = 0;
+    public static void main(String[] args){
+    System.out.println("Enter an expression (or \"quit\"): "); //prompts user for input
+    intro();
+
+}
+
+    public static void intro(){
+        Scanner input = new Scanner(System.in); 
+        String user= input.nextLine();
+        while (!user.equals("quit") & input.hasNextLine()){ //processes code when user input does not equal quit
+            StringTokenizer chunks = new StringTokenizer(user, " "); //parses by white space
+            String fraction1 = chunks.nextToken(); //first fraction
+            String operand = chunks.nextToken(); //operator 
+            String fraction2 = chunks.nextToken(); //second fraction
+            System.out.println("Fraction 1: " + fraction1); 
+            System.out.println("Operation: " + operand); 
+            System.out.println("Fraction 2: " + fraction2); 
+            System.out.println("Enter an expression (or \"quit\"): "); //prompts user for more input
+
+            while (user.contains("*")){
+                parse(fraction1);
+                parse(fraction2);
+                System.out.println("hi");
+                int num = num1 * num2;
+                int denom = denom1 * denom2;
+                System.out.println(num + "/" + denom);
+                user = input.next();
+
+            }
+    
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner scan = new Scanner(System.in);
+    	scan.nextLine();
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
